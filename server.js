f1d1a6495bae7578591238470d1ecb482dff9099
onoroff <adam.grunke@gmail.com>
2019-07-16 20:20:37 -0700

    routes mapped out. Not all tested or finalized

--- server.js
+++ server.js
@@ -37,16 +37,60 @@ app.post('/camera', (req, res) => {
 // Model.findByIdAndUpdate(id, { name: 'jason bourne' }, options, callback)
 // UPDATE - parent
 app.put('/camera/:id', (req, res) => {
-    Camera.findByIdAndUpdate(id, {megaPixels: 80}, function(err, camera) {
+    Camera.findByIdAndUpdate(req.params.id, {megaPixels: 80}, function(err, camera) {
         if (err) res.json(err)
-            res.json(camera)
+        res.json(camera)
     })
 })
 
 // DELETE - parent
-app.get('/camera/delete', (req, res) => {
-    User.findOneAndRemove({model: "G9"}, function(err) {
+app.delete('/camera', (req, res) => {
+    Camera.findOneAndRemove({model: "G9"}, function(err) {
         if (err) res.json(err);
         res.json({message: "Camera Deleted"})
     })
 })
+
+// Child CRUD ROUTES
+// GET ONE
+app.get('/lens/:id', (req, res) => {
+    Lens.findById(req.params.id, function(err, lens) {
+        if (err) res.json(err)
+        res.json(lens)
+    })
+})
+
+// CREATE ONE - placeholder
+app.post('/lens/:id', (req, res) => {
+    Camera.findById(req.params.id, function (err, camera) {
+    Lens.findById(req.body.id, function (err, lens) {
+        camera.lens.push(lens);
+        camera.save(function (err) {
+        camera.lens.push(lens)
+        lens.save(function (err) {
+            if (err) res.json(err)
+            res.json(lens)
+        })
+        })
+    })
+    })
+})
+
+// DELETE - placeholder
+app.post('/lens/:id', (req, res) => {
+    Lens.findById(req.params.id, function (err, lens) {
+    Camera.findById(req.body.id, function (err, camera) {
+        camera.lens.push(lens);
+        camera.save(function (err) {
+        camera.lens.push(lens)
+        lens.save(function (err) {
+            if (err) res.json(err)
+            res.json(lens)
+        })
+        })
+    })
+    })
+})
+
+
+
